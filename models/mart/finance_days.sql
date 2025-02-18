31160f29fd3dfe3cd67d2fe2baa56a0cc3964a3b
Maxime <maximebarbier57@gmail.com>
2025-02-18 14:34:19 +0000

    challenge6_test_ok

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -7,7 +7,7 @@ SELECT
     ,ROUND(SUM(operational_margin),2) AS operational_margin
     ,ROUND(SUM(purchase_cost),2) AS total_purchase_cost
     ,ROUND(SUM(shipping_fee) ,2)AS total_shipping_fee
-    ,ROUND(sum(log_cost),2) AS log_cost
+    ,ROUND(sum(log_cost),2) AS total_log_cost
     ,SUM(quantity) AS total_pdt_sold
 FROM {{ ref('int_orders_operational') }}
 GROUP BY date_date
