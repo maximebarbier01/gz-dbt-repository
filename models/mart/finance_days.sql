84c2f81780f59bde163f2a5cd9f03a5c2237516e
Maxime <maximebarbier57@gmail.com>
2025-02-18 13:44:04 +0000

    Add_financial_days

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,13 +1,14 @@
 SELECT
     date_date
     ,COUNT(orders_id) AS nb_transactions
-    ,SUM(revenue) AS revenue
-    ,AVG(revenue) AS average_basket
-    ,SUM(operational_margin) AS operational_margin
-    ,SUM(purchase_cost) AS purchase_cost
-    ,SUM(shipping_fee) AS total_shipping_fee
-    ,sum(log_cost) AS log_cost
+    ,ROUND(SUM(revenue),2) AS revenue
+    ,ROUND(AVG(revenue),2) AS average_basket
+    ,ROUND(SUM(margin),2) AS margin
+    ,ROUND(SUM(operational_margin),2) AS operational_margin
+    ,ROUND(SUM(purchase_cost),2) AS total_purchase_cost
+    ,ROUND(SUM(shipping_fee) ,2)AS total_shipping_fee
+    ,ROUND(sum(log_cost),2) AS log_cost
     ,SUM(quantity) AS total_pdt_sold
 FROM {{ ref('int_orders_operational') }}
 GROUP BY date_date
-ORDER BY date_date
+ORDER BY date_date DESC
