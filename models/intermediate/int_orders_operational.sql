dd226f473843e444c5a12eab92585df8e8a39cc8
Maxime <maximebarbier57@gmail.com>
2025-02-18 13:18:17 +0000

    Calculate the Operational Margin per Order_add_values

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,6 +1,10 @@
 SELECT 
     orders_id
     ,date_date
+    ,SUM(quantity) AS nb_transactions
+    ,ROUND(SUM(revenue),2) AS revenue
+    ,ROUND(AVG(margin),2) AS average_basket
+    ,ROUND(SUM(margin),2) AS margin
     ,ROUND(SUM(margin + shipping_fee - log_cost - ship_cost),2) AS operational_margin
 FROM {{ ref('int_orders_margin') }}
 JOIN {{ ref('ship') }}
