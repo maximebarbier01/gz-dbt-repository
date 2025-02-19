9f7c912d87577b73da2c2528ed967b9a58444a80
Maxime Barbier <maximebarbier57@gmail.com>
2025-02-19 08:31:21 +0000

    correction_with_solutions

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -3,5 +3,5 @@ SELECT
     ,quantity * CAST(purchase_price AS FLOAT64) AS purchase_cost
     ,ROUND(revenue - quantity * CAST(purchase_price AS FLOAT64),2) AS margin
 FROM {{ ref('sales') }}
-JOIN {{ ref('product') }}
+LEFT JOIN {{ ref('product') }}
 USING(products_id)
